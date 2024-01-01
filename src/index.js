9435e6b006256a07ac5f775e78fcde51727c0b5c
sjalcidor <120965448+sjalcidor@users.noreply.github.com>
2023-12-31 21:02:16 -0500

    Added AI and API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+function displayPoem(response){
+console.log("poem generated");
+    new Typewriter('#poem', {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+}
+
 function generatePoem(event) {
     event.preventDefault();
 
 let poemElement = document.querySelector("#poem");
 
-new Typewriter('#poem', {
-    strings: "Words Here",
-    autoStart: true,
-  });
+let instructionsInput = document.querySelector("#instructions");
+
+
+let apiKey= "20811777afcctfb386aebf0oe8a480fb";
+let context = "You are a Poem expert. Your mission is to generate a romantic or funny, 4 line poem in basic HTML. Make sure to follow the user instructions.";
+let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}
+`;
+console.log("Generating Poem");
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+
+axios.get(apiUrl).then(displayPoem);
+
 
 
 }
